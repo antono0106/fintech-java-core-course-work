ad470350b0d05e33d9579b187a60af87991f5c34
antono0106 <antono0106@gmail.com>
2022-02-01 01:26:33 +0300

    persistence initial commit

---
+++ src/main/java/com/moroz/logging/CustomLogger.java
@@ -0,0 +1,19 @@
+package com.moroz.logging;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @author : anton
+ * @since : 01.02.2022, вт
+ **/
+public class CustomLogger {
+    private static final File file = new File("logs.log");
+
+    /*TODO: create file in resource directory
+        https://stackoverflow.com/questions/8513729/create-file-in-resources-source-folder-in-java-programmatically/58095374
+    */
+    public static void info(String info) {
+
+    }
+}
