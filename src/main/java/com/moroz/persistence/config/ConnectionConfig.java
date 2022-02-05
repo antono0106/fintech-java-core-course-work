669459bffb6169ebac2713e8e730def21f0c2e20
antono0106 <antono0106@gmail.com>
2022-02-05 22:20:52 +0300

    finished connection config

--- src/main/java/com/moroz/persistence/config/ConnectionConfig.java
+++ src/main/java/com/moroz/persistence/config/ConnectionConfig.java
@@ -3,6 +3,8 @@ package com.moroz.persistence.config;
 import com.moroz.logging.CustomLogger;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 /**
@@ -10,15 +12,36 @@ import java.util.Properties;
  * @since : 01.02.2022, вт
  **/
 public class ConnectionConfig {
+    private static final String url = "jdbc:postgresql://localhost:5432/cinema";
+    private static final Properties props = new Properties();
     private static Connection connection;
-    private final Properties props = new Properties();
 
+    static final CustomLogger logger = new CustomLogger(ConnectionConfig.class);
 
-    private ConnectionConfig() {
-
+    public static Connection getInstance() {
+        if (connection == null) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                props.setProperty("user", "postgres");
+                props.setProperty("password", "root");
+                connection = DriverManager.getConnection(url, props);
+                logger.info("Connected to " + connection.getCatalog());
+            } catch (SQLException | ClassNotFoundException e) {
+                logger.error(e.getMessage(), e);
+                throw new RuntimeException("Failed to connect to database");
+            }
+        }
+        return connection;
     }
 
-    public Connection getInstance() {
-        return connection;
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                logger.info("Connection closed successfully");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ src/test/java/com/moroz/ConnectionTest.java
@@ -0,0 +1,21 @@
+package com.moroz;
+
+import com.moroz.persistence.config.ConnectionConfig;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.Assert.*;
+
+public class ConnectionTest {
+
+    @Test
+    public void appMustBeConnectedToDb() throws SQLException {
+        Connection connection = ConnectionConfig.getInstance();
+        assertNotNull(connection);
+        assertFalse(connection.isClosed());
+        connection.close();
+        assertTrue(connection.isClosed());
+    }
+}
